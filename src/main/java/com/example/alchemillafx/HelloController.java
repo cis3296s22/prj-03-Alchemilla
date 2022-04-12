f7a1cb661d32df6170be11b385174a7ace4b2b05
Alexander-Russakoff <tuo49330@temple.edu>
2022-04-12 15:59:39 -0400

    Now correctly updating the screen with plant image and plant information dynamically generated from the default profile!!!!!!

--- src/main/java/com/example/alchemillafx/HelloController.java
+++ src/main/java/com/example/alchemillafx/HelloController.java
@@ -2,16 +2,18 @@ package com.example.alchemillafx;
 
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
+import javafx.embed.swing.SwingFXUtils;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.image.ImageView;
 
+
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.net.URL;
-import java.util.ArrayList;
+
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.ResourceBundle;
@@ -28,9 +30,9 @@ public class HelloController implements Initializable {
     private ImageView plantImage;
 
     String currentProfile = "joe"; //TODO currently hardcoded for testing, obviously cannot stay like this
-    String currentPlant = "";
+    String currentPlantName;
     LinkedList<String> plants = new LinkedList<String>();
-
+    Profile profile;
 
     @FXML
     private void displayPlant(BufferedImage plant){
@@ -43,7 +45,7 @@ public class HelloController implements Initializable {
     public void initialize(URL arg0, ResourceBundle arg1){
         SerializeClassData sc = new SerializeClassData();
         try {
-            Profile profile = sc.getProfile(currentProfile);
+            profile = sc.getProfile(currentProfile);
             LinkedList<Plant> plantLinkedList = profile.getPlants();
             Iterator i = plantLinkedList.iterator();
             while (i.hasNext()){
@@ -52,9 +54,7 @@ public class HelloController implements Initializable {
                plants.add(tempPlant.getPlantName());
 
             }
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (ClassNotFoundException e) {
+        } catch (IOException | ClassNotFoundException e) {
             e.printStackTrace();
         }
 
@@ -62,8 +62,27 @@ public class HelloController implements Initializable {
 
             @Override
             public void changed(ObservableValue<? extends String> arg0, String arg1, String arg2){
-                currentPlant = plantList.getSelectionModel().getSelectedItem();
-                plantData.setText(currentPlant);
+
+                currentPlantName = plantList.getSelectionModel().getSelectedItem();
+                plantData.setText(currentPlantName);
+
+                //iterate through plants to get image information
+                LinkedList<Plant> lp = profile.getPlants();
+                Iterator i = lp.iterator();
+
+                while(i.hasNext()){
+                    Plant p = (Plant) i.next();
+                    if (p.getPlantName().equals(currentPlantName)){
+                        try {
+
+
+                            plantImage.setImage(SwingFXUtils.toFXImage(p.getPlantImage(),null));
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+
             }
 
         });
--- src/main/java/com/example/alchemillafx/Plant.java
+++ src/main/java/com/example/alchemillafx/Plant.java
@@ -48,6 +48,8 @@ public class Plant implements Serializable {
 
         return ImageIO.read( Plant.class.getResource(plantImage));
     }
+    
+    
 
     public String getPlantName() {
         return plantName;
