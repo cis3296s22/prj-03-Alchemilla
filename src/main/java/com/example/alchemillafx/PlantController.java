525543d7a56c026b1f04e45674092a773ca73fbe
jason-zhang-github <zhang.jason.2021@gmail.com>
2022-04-14 21:32:58 -0400

    plant ui

--- src/main/java/com/example/alchemillafx/PlantController.java
+++ src/main/java/com/example/alchemillafx/PlantController.java
@@ -7,11 +7,43 @@ import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import javafx.scene.control.TextField;
+import javafx.scene.text.*;
 
+import java.awt.image.BufferedImage;
+import java.io.BufferedReader;
 import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import javax.imageio.ImageIO;
 
 public class PlantController extends Application
 {
+    @FXML
+    private Text actiontarget;
+
+    @FXML
+    private TextField plantname;
+
+    @FXML
+    private TextField water;
+
+    @FXML
+    private TextField fooddelta;
+
+    @FXML
+    private TextField food;
+
+    @FXML
+    private TextField description;
+
+    @FXML
+    private TextField instruction;
+
+    @FXML
+    private TextField plantimagepath;
+
+
 
     @Override
     public void start(Stage primaryStage) throws Exception
@@ -31,11 +63,33 @@ public class PlantController extends Application
     }
 
     @FXML
-    private Label welcomeText;
-
-    @FXML
     protected void onButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+        actiontarget.setText("Welcome to JavaFX Application!");
+
+        String imageString = plantimagepath.getText();
+        String nameString = plantname.getText();
+        String waterString = water.getText();
+        String foodDeltaString = fooddelta.getText();
+        String foodString = food.getText();
+        String descString = description.getText();
+        String instructionString = instruction.getText();
+
+
+        // NEEDS PLANT IMAGE PATH ADAPTER
+        Plant plant = new Plant(plantimagepath, nameString, Integer.parseInt(waterString), Integer.parseInt(foodDeltaString), foodString, descString, instructionString);
+    }
+
+    private BufferedImage (String path)
+    {
+        try
+        {
+            BufferedImage image = ImageIO.read();
+
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
     }
 
     public static void main (String[] args)
