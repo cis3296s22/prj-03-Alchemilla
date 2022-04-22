a4ab39efbc2763a7fc9c27263f8f7a1486884894
Akshay Varghese <tug07790@temple.edu>
2022-04-21 23:57:27 -0400

    Comments added to all tests in ProfileTest. New testing file added for Plant class.

---
+++ src/main/java/com/example/alchemillafx/PlantTest.java
@@ -0,0 +1,40 @@
+package com.example.alchemillafx;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PlantTest {
+
+    @Test
+    public void getInstructions() {
+    }
+
+    @Test
+    public void getDescription() {
+    }
+
+    @Test
+    public void getPlantImage() {
+    }
+
+    @Test
+    public void getPlantName() {
+    }
+
+    @Test
+    public void getWaterDelta() {
+    }
+
+    @Test
+    public void getFoodDelta() {
+    }
+
+    @Test
+    public void getImagePath() {
+    }
+
+    @Test
+    public void getFood() {
+    }
+}
--- src/main/java/com/example/alchemillafx/ProfileTest.java
+++ src/main/java/com/example/alchemillafx/ProfileTest.java
@@ -31,12 +31,18 @@ public class ProfileTest {
         cal = new GregorianCalendar();
     }
 
+    /**
+     * Call to display should display proper name.
+     */
     @org.junit.Test
     public void shouldDisplayProfileName() {
         assertEquals(str, prof.getName(),
                 "Should display \"TestProfile\" as name of profile:");
     }
 
+    /**
+     * Call change name should alter profile name.
+     */
     @org.junit.Test
     public void shouldDisplayNewName() {
         String strNew = "NewTestProfile";
@@ -45,12 +51,18 @@ public class ProfileTest {
                 "Should display \"NewTestProfile\" as the new name of profile:");
     }
 
+    /**
+     * Call to display plants should return null plant list if none added.
+     */
     @org.junit.Test
     public void shouldReturnNullWithNoPlants() {
         assertEquals(plants, prof.getPlants(),
                 "Should have a null LinkedList of Plant objects:");
     }
 
+    /**
+     * Call to display plants should return plant list with one item if added.
+     */
     @org.junit.Test
     public void shouldReturnPlantWhenPlantAdded() {
         prof.addPlant(plant1);
@@ -59,6 +71,9 @@ public class ProfileTest {
                 "Should have a single LinkedList of Plant objects:");
     }
 
+    /**
+     * Call to display plants should return plant list with more than one item if added.
+     */
     @org.junit.Test
     public void shouldReturnArrayListOfPlantsWhenPlantsAdded() {
         prof.addPlant(plant1);
@@ -71,12 +86,18 @@ public class ProfileTest {
                 "Should have a single LinkedList of Plant objects:");
     }
 
+    /**
+     * Call to display start date should display no start date if none set.
+     */
     @org.junit.Test
     public void shouldReturnNullStartDateWhenNoneSet() {
         assertEquals(cal, prof.getStartDate(),
                 "Should have a single LinkedList of Plant objects:");
     }
 
+    /**
+     * Call to display start date should display it if one is set.
+     */
     @org.junit.Test
     public void shouldReturnStartDateWhenSet() {
         cal.set(Calendar.YEAR, Calendar.JANUARY, Calendar.SATURDAY);
@@ -85,6 +106,9 @@ public class ProfileTest {
                 "Should have a single LinkedList of Plant objects:");
     }
 
+    /**
+     * Call to display start date should display it if one set through constructor.
+     */
     @org.junit.Test
     public void shouldReturnStartDateFromConstruction() {
         cal.set(Calendar.YEAR, Calendar.JANUARY, Calendar.SATURDAY);
